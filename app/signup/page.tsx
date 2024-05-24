a46db026dbae3bbca9a1b79f7342a64b7861f98b
nibilmathew <nibilmathew97@gmail.com>
2024-05-24 09:18:24 +0530

    landing page

---
+++ app/signup/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex felx-col min-h-screen item-center justify-center w-full felx-1 px-20 text-center bg-white">
+    <div className="bg-white rounded-2xl shadow-2xl flex w-2/3max-w-4xl">
+  <div className="w-3/5 p-5">
+    <div className="py-10">
+        <h2 className="text-3xl font-bold text-green-500 mb-2">sign up</h2>
+    <div className="border-2 w-10 border-green-500 inline-block mb-2"></div>
+    <div className="flex justify-center my-2">
+        
+    </div>
+    <p className="text-gray-400 my-3"> or use ur email acct</p>
+    box
+    boxnpm
+    </div>
+  </div>
+  <div className="w-2/5 bg-green-500 text-whit erounded-tr-2xl rounded-br-2xl py-36 px-12">pic</div>
+  <h2 className="text-3xl font-bold mb-2"> hello</h2>
+  <div className="boreder-2 w-10 border-white inline-block mb-2"></div>
+  <p className="mb-2">do u have an acct</p>
+  <a href="" className="border-2 border-white rounded-full px-2 py-2 inline-block font-semibold hover: bg-white hover:text-green-500"></a>
+  
+  </div>
+  </div>
+  );
+}
