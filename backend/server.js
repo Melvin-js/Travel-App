7f87da7c4a2c5310664e10b6ba3f1952b8f40177
nibilmathew <nibilmathew97@gmail.com>
2024-06-26 19:44:10 +0530

    firebase

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.static(path.join(__dirname)));
 app.use(bodyParser.json());
